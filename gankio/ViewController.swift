829b5e68079cbb9ef82cb35dba41da3f72bfae35
yuhaocan <haocan.yu@huijie-inc.com>
2020-01-27 13:52:04 +0800

    Initial Commit

---
+++ gankio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gankio
+//
+//  Created by yuhaocan on 2020/1/27.
+//  Copyright © 2020 yuhaocan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
